9ba23bede0db1cdcbad331aced4f57e0aa7ecf31
Torrey Kanko <torphus101@gmail.com>
2021-08-30 13:18:23 -0700

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf)
+    dwarf.each.with_index do |name, idx|
+      puts "#{idx + 1} + #{name}"
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(summon)
+    summon.collect do |element|
+      element.capitalize + "!"
+    end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+    calls.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ray_str)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ray_str.detect{|cheese| cheese_types.include?(cheese)}
 end
